5c0f52f848899e8d897cd242a14b1553d9ab99a7
ozzy <businessozzy27@gmail.com>
2022-10-26 20:36:33 -0400

    Stole my friend's version (neltils)
    https://gist.githubusercontent.com/Ultra-bob/9f6a86af980808b4861e5cfda1a1ae04/raw/7bf88bf9392aef51a9c39defa4834db3da6c1521/neltils.py
    
    more functions added lol

--- oztils.py
+++ oztils.py
@@ -29,3 +29,37 @@ def isPalindrome(check):
 #turns list into string
 def listToString(listInp):
 	return ''.join(listInp)
+
+def cache(func):
+    cache = {}
+
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = func(*args)
+            return cache[args]
+    
+    return wrapper
+
+def ns_per_sec(num):
+    return num * 10 ** 9 # 10 to the 9 is 1 billion
+
+def replace_multiple(text, table):
+    """replace using a dictionary
+    Args:
+        text ([string]): [text to be replaced]
+        table ([dictionary]): [dictionary with keys as the target and values as what to replace targets with]
+    """
+
+    for key in table.keys():
+        text = text.replace(key, table[key])
+
+    return text
+
+def slowprint(text, delay):
+    from time import sleep
+    for char in text:
+        print(char, end="",flush=True)
+        sleep(delay)
+    print()
