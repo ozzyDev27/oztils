897457220d496cf9b5fd6c8161c301f9c5192248
ozzy <businessozzy27@gmail.com>
2022-11-01 11:43:39 -0400

    Removed a tab error comments and formatting
    No more comments - will add documentation

--- oztils.py
+++ oztils.py
@@ -1,50 +1,23 @@
 import os
-
-#range(3) returns [0,1,2], where nonIndexedRange(3) returns [1,2,3]
 def nonIndexedRange(rangeInp):
 	lst = list(range(int(rangeInp)))
 	for i in lst: lst[i]+=1
 	return lst
-
-#If input "Hello", returns "olleH" - if input 125, return 521. If input ["world!", "Hello,"], return ["Hello,", "world!"]
-def invert(strInp):
-	return strInp[::-1]
-
-#Removes version compatability - removeStart("Hello! ", "Hello! My name is Bill!") returns "My name is Bill!"
+def invert(strInp):return strInp[::-1]
 def removeStart(start, strInp):
 	if strInp.startswith(str(start)):
 		startLength=len(str(start))
 		return strInp[startLength:]
-	else:
-		return strInp
+	else:return strInp
 def removeEnd(end, strInp):
 	if strInp.endswith(str(end)):
 		endLength=len(str(end))
 		return strInp[:-endLength]
-	else:
-		return strInp	
-#Checks if an input (string, int, list) is a palindrome
+	else:return strInp
 def isPalindrome(check):
 	if check==check[::-1]: return True
 	else: return False
-
-#turns list into string
-def listToString(listInp):
-	return ''.join(listInp)
-
-def cache(func):
-    cache = {}
-
-    def wrapper(*args):
-        if args in cache:
-            return cache[args]
-        else:
-            cache[args] = func(*args)
-            return cache[args]
-    
-    return wrapper
-
-
+def listToString(listInp):return ''.join(listInp)
 def slowprint(text, delay):
     from time import sleep
     for char in text:
@@ -55,7 +28,6 @@ def slowprint(text, delay):
 def remove(strIn, toRemove):
 	for char in toRemove: strIn=strIn.replace(char,'')
 	return strIn
-
 def fileAppend(file,toWrite):
 	localOpen = open(file, "a")
 	localOpen.write(str(toWrite))
@@ -73,10 +45,8 @@ def fileRename(file,toRename):
 def last(listName):
 	return listName[len(listName)-1]
 def isRound(num):
-	if round(num)==num:
-		return True
-	else:
-		return False
+	if round(num)==num:return True
+	else:return False
 def intput(ina):
 	try: return int(input(ina))
 	except: return False
@@ -96,6 +66,6 @@ def invBool(bo):
 	if bo==True: return False
 	else: return True
 def isPrime(n):
-		for i in range(2,round(n/2)+2):
-			if (n%i) == 0:return False
-		return True
+	for i in range(2,round(n/2)+2):
+		if (n%i) == 0:return False
+	return True
