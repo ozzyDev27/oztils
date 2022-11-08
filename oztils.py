89d23eb69326a385f81451019a1d509f8d571b90
ozzy <businessozzy27@gmail.com>
2022-11-08 10:28:01 -0500

    Fixed version compatability oversight

--- oztils.py
+++ oztils.py
@@ -1,4 +1,4 @@
-def nonIndexedRange(rangeInp: int): return list(range(1,int(rangeInp)+1))
+def nonIndexedRange(rangeInp): return list(range(1,int(rangeInp)+1))
 def invert(strInp):return str(strInp)[::-1]
 def removeStart(start, strInp):
 	if strInp.startswith(str(start)):
@@ -13,7 +13,7 @@ def removeEnd(end, strInp):
 def isPalindrome(check):
 	if str(check)==str(check)[::-1]: return True
 	else: return False
-def reverseList(ins: list):
+def reverseList(ins):
 	return list(ins)[::-1]
 def listToString(listInp):return ''.join(listInp)
 def slowprint(text: str, delay):
@@ -25,27 +25,27 @@ def slowprint(text: str, delay):
 def remove(strIn, toRemove):
 	for char in toRemove: strIn=strIn.replace(char,'')
 	return strIn
-def fileAppend(file: str,toWrite: str):
+def fileAppend(file,toWrite):
 	import os
 	localOpen = open(file, "a")
 	localOpen.write(str(toWrite))
 	localOpen.close()
-def fileErase(file: str):
+def fileErase(file):
 	import os
 	localOpen = open(file, "w")
 	localOpen.write("")
 	localOpen.close()
-def fileDelete(file: str):
+def fileDelete(file):
 	import os
 	os.remove(file)
-def fileRename(file: str,toRename: str):
+def fileRename(file,toRename):
 	import os
 	os.rename(file,toRename)
 def isRound(num):return round(num)==num
 def intput(ina):
 	try: return int(input(ina))
 	except: return False
-def simplify(a: int,b: int):
+def simplify(a,b):
 	factsA=[]
 	for checkA in range(1,round(a/2+1)):
 		if a/checkA==round(a/checkA):factsA.append(checkA)
@@ -57,11 +57,11 @@ def simplify(a: int,b: int):
 			facts.append(_)
 			break
 	return [int(a/facts[len(facts)-1]),int(b/facts[len(facts)-1])]
-def isPrime(n: int):
+def isPrime(n):
 	for i in range(2,round(n/2)+2):
 		if (n%i) == 0:return False
 	return True
-def deleteAllOf(ls: list,target):
+def deleteAllOf(ls,target):
 	while target in ls:
 		ls.pop(ls.index(target))
 	return ls
@@ -72,7 +72,7 @@ def abs(n):
 	else: 
 		try:return int(n)
 		except: return n
-def factorial(k: int):
+def factorial(k):
 	for i in range(k-1):k*=(k-i)
 	return k
 def bound(num,maxn,minn):sorted((minn, num, maxn))[1]
