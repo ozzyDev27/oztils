5d984760adb89b71fa4af422010bb470a9c8bac5
ozzy <businessozzy27@gmail.com>
2022-11-27 11:47:12 -0500

    Fixed floor and ciel bug
    Sometimes returned 0

--- oztils.py
+++ oztils.py
@@ -82,5 +82,5 @@ def xor(a,b):return (a or b) and (not a and b)
 def mustInput(s,w):
 	n=input(s)
 	while not n in w:n=input(s)
-def ceil(n):return round(n)+1 if round(n)<n else 0
-def floor(n):return round(n)+1 if round(n)>n else 0
+def ceil(n):return round(n)+1 if round(n)<n else round(n)
+def floor(n):return round(n)+1 if round(n)>n else round(n)
